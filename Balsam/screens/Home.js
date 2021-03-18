447a24b4e95fd3aaace68cc231fc0e9c08f7b226
Nabeel20 <nabeelandnizam@gmail.com>
2021-03-18 11:49:16 +0200

    Fix Icons in Home.js

--- Balsam/screens/Home.js
+++ Balsam/screens/Home.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { View, Text, StyleSheet, FlatList } from 'react-native'
 import { TouchableRipple, Surface, Dialog, Portal, Divider, IconButton, Button, useTheme } from 'react-native-paper'
 import { createStackNavigator } from '@react-navigation/stack';
-import Octicons from 'react-native-vector-icons/Octicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { FileSystem } from 'react-native-file-access';
 import * as Animatable from 'react-native-animatable';
 import * as Haptics from 'expo-haptics';
@@ -23,8 +23,8 @@ export default function Home({ navigation }) {
     function EmptyHome() {
         return (
             <Animatable.View animation="fadeIn" style={{ alignItems: 'center', justifyContent: 'center', flex: 1, width: '100%' }}>
-                <Octicons
-                    name='file-symlink-file'
+                <MaterialCommunityIcons
+                    name='file-download'
                     color='grey'
                     size={50} style={{ marginLeft: 5 }} />
                 <Text style={{ fontFamily: 'Cairo_700Bold', color: 'grey' }}>جرّب إضافة بعض الملفات</Text>
@@ -86,15 +86,15 @@ export default function Home({ navigation }) {
         }
         function get_icon(quiz) {
             if (quiz.is_cycle()) {
-                return { name: 'verified', color: colors.error }
+                return { name: 'check-decagram', color: '#E53935' }
             }
             else if (quiz.index > 0) {
-                return { name: 'stop', color: 'grey' }
+                return { name: 'asterisk', color: 'grey' }
             }
             else if (quiz.taken_number > 0) {
-                return { name: 'dot-fill', color: 'grey' }
+                return { name: 'checkbox-blank-circle', color: 'grey' }
             }
-            return { name: 'dot', color: 'grey' }
+            return { name: 'checkbox-blank-circle-outline', color: 'grey' }
         }
         return (
             <View style={styles.container}>
@@ -129,7 +129,7 @@ export default function Home({ navigation }) {
                                         <View>
                                             <Text style={styles.title}>{item.title}</Text>
                                             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                                <Octicons
+                                                <MaterialCommunityIcons
                                                     name={get_icon(item).name}
                                                     color={get_icon(item).color}
                                                     size={16} style={{ marginLeft: 5 }} />
@@ -142,11 +142,11 @@ export default function Home({ navigation }) {
                                         <View>
                                             <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
                                                 <Text style={styles.numbers}>{item.get_questions_number()}</Text>
-                                                <Octicons name="list-ordered" size={16} color="grey" style={{ marginLeft: 5 }} />
+                                                <MaterialCommunityIcons name="format-list-numbered" size={16} color="grey" style={{ marginLeft: 5 }} />
                                             </View>
                                             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                                                 <Text style={styles.numbers}>{item.get_estimated_time()}</Text>
-                                                <Octicons name="clock" size={16} color="grey" style={{ marginLeft: 5 }} />
+                                                <MaterialCommunityIcons name="progress-clock" size={16} color="grey" style={{ marginLeft: 5 }} />
                                             </View>
                                         </View>
 
@@ -183,7 +183,7 @@ export default function Home({ navigation }) {
                         <Dialog.Content style={{ margin: 3, padding: 3 }}>
                             <View style={[styles.row, { justifyContent: 'space-between' }]}>
                                 <View style={styles.row}>
-                                    <Octicons name='repo' size={16} style={{ marginRight: 3 }} color='grey' />
+                                    <MaterialCommunityIcons name='target-variant' size={16} style={{ marginRight: 3 }} color='grey' />
                                     <Text style={styles.dialog_text}>متوسط التحصيل في المقرر</Text>
                                 </View>
                                 <Text style={styles.dialog_text}>{dialogData.average_accuracy}</Text>
@@ -191,7 +191,7 @@ export default function Home({ navigation }) {
                             <Divider />
                             <View style={[styles.row, { justifyContent: 'space-between' }]}>
                                 <View style={styles.row}>
-                                    <Octicons name='history' size={16} style={{ marginRight: 3 }} color='grey' />
+                                    <MaterialCommunityIcons name='history' size={16} style={{ marginRight: 3 }} color='grey' />
                                     <Text style={styles.dialog_text}>متوسط الوقت في المقرر</Text>
                                 </View>
                                 <Text style={styles.dialog_text}>{dialogData.average_time}</Text>
@@ -199,7 +199,7 @@ export default function Home({ navigation }) {
                             <Divider />
                             <View style={[styles.row, { justifyContent: 'space-between' }]}>
                                 <View style={styles.row}>
-                                    <Octicons name='calendar' size={16} style={{ marginRight: 3 }} color='grey' />
+                                    <MaterialCommunityIcons name='calendar-today' size={16} style={{ marginRight: 3 }} color='grey' />
                                     <Text style={styles.dialog_text}>آخر مرة </Text>
                                 </View>
                                 <Text style={styles.dialog_text}>{calculate_last_time(dialogData.last_time)}</Text>
@@ -207,7 +207,7 @@ export default function Home({ navigation }) {
                             <Divider />
                             <View style={[styles.row, { justifyContent: 'space-between' }]}>
                                 <View style={styles.row}>
-                                    <Octicons name='file-badge' color='#313131' size={16} style={{ marginRight: 3 }} color='grey' />
+                                    <MaterialCommunityIcons name='file-check' color='#313131' size={16} style={{ marginRight: 3 }} color='grey' />
                                     <Text style={styles.dialog_text}>آخر نتيجة</Text>
                                 </View>
                                 <Text style={styles.dialog_text}>% {dialogData.last_score}</Text>
@@ -215,7 +215,7 @@ export default function Home({ navigation }) {
                             <Divider />
                             <View style={[styles.row, { justifyContent: 'space-between' }]}>
                                 <View style={styles.row}>
-                                    <Octicons name='stopwatch' size={16} style={{ marginRight: 3 }} color='grey' />
+                                    <MaterialCommunityIcons name='clock-check' size={16} style={{ marginRight: 3 }} color='grey' />
                                     <Text style={styles.dialog_text}>آخر توقيت</Text>
                                 </View>
                                 <Text style={styles.dialog_text}>{dialogData.last_time_score} د</Text>
@@ -242,7 +242,7 @@ export default function Home({ navigation }) {
                 options={{
                     title: 'بلســم',
                     headerTitleStyle: { fontFamily: 'Cairo_700Bold' },
-                    headerLeft: () => (<Octicons size={24} style={{ marginLeft: 20 }} name='three-bars' onPress={() => navigation.openDrawer()} />)
+                    headerLeft: () => (<MaterialCommunityIcons size={24} style={{ marginLeft: 20 }} name='menu' onPress={() => navigation.openDrawer()} />)
                 }} />
             <Stack.Screen
                 name="Exam"
