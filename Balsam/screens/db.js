938e41b36625fb3d4b155a827b84ab0618cc703b
Nabeel20 <nabeelandnizam@gmail.com>
2021-03-18 18:09:29 +0200

    adding test

--- Balsam/screens/db.js
+++ Balsam/screens/db.js
@@ -4,7 +4,7 @@ import CryptoJS from 'crypto-js';
 
 let database = []
 let bookmarks = []
-let act = []
+let act_array = []
 let error_array = []
 function get_database() {
     return database
@@ -26,17 +26,17 @@ async function update_bookmarks(update) {
 }
 
 function get_act() {
-    return act
+    return act_array
 }
 function is_quiz_valid(code) {
-    if ([...new Set(act)].filter(i => i.code == code)[0].valid) return true
+    if ([...new Set(act_array)].filter(i => i.code == code)[0].valid) return true
     return false
 }
 async function update_act(data) {
-    act = data;
+    act_array = data;
     try {
-        const jsonValue = JSON.stringify(act)
-        await AsyncStorage.setItem('@act', CryptoJS.AES.encrypt(jsonValue, 'nabeeladnanalinizam_20900!@#()').toString())
+        const jsonValue = JSON.stringify(act_array)
+        await AsyncStorage.setItem('@act_array', CryptoJS.AES.encrypt(jsonValue, 'nabeeladnanalinizam_20900!@#()').toString())
     } catch (e) {
     }
 }
